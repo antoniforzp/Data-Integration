60a27006552afb84cc3cd67870e0964b3a55f4e0
jedrek <jedrekszor@wp.pl>
2020-05-27 17:16:29 +0100

    edit xml node

--- Logic/src/main/java/model/logic/XMLManipulationLogic.java
+++ Logic/src/main/java/model/logic/XMLManipulationLogic.java
@@ -1,12 +1,16 @@
 package model.logic;
 
 import model.data.Movie;
+import model.resources.XMLJDomFunctions;
 import model.resources.XPathFunctions;
 import net.sf.saxon.s9api.SaxonApiException;
+import net.sf.saxon.s9api.XdmItem;
 import net.sf.saxon.s9api.XdmValue;
 import org.jdom2.Attribute;
 import org.jdom2.Document;
 import org.jdom2.Element;
+
+import java.util.Iterator;
 
 public class XMLManipulationLogic {
     public static Document addMovie(Movie movie, Document doc) {
@@ -23,50 +27,7 @@ public class XMLManipulationLogic {
             String xp = "//movie[title=\"" + movie.getTitle() + "\"]";
             XdmValue res = XPathFunctions.executeXpath(xp, "movies.xml");
             if (res == null || res.size() == 0) {
-                Element newMovie = new Element("movie");
-                Element title = new Element("title").addContent(movie.getTitle());
-                Element cover = new Element("cover").addContent(movie.getCover());
-                Element year = new Element("year").addContent(String.valueOf(movie.getYear()));
-                Element releaseDate = new Element("releaseDate").addContent(String.valueOf(movie.getReleaseDate()));
-                Attribute releaseCountry = new Attribute("country", "USA");
-                releaseDate.setAttribute(releaseCountry);
-                Element countries = new Element("productionCountries");
-                for (String s : movie.getCountry()) {
-                    Element country = new Element("country").addContent(s);
-                    countries.addContent(country);
-                }
-                Element director = new Element("director").addContent(movie.getDirector());
-                Element cast = new Element("cast");
-                for (String s : movie.getCast()) {
-                    Element actor = new Element("actor").addContent(s);
-                    cast.addContent(actor);
-                }
-                Element duration = new Element("duration").addContent(String.valueOf(movie.getDuration()));
-                Element distribution = new Element("distribution").addContent(movie.getDistribution());
-                Element languages = new Element("languages");
-                for (String s : movie.getLanguage()) {
-                    Element language = new Element("language").addContent(s);
-                    languages.addContent(language);
-                }
-                Element music = new Element("music").addContent(movie.getMusic());
-                Element boxOffice = new Element("boxOffice").addContent(String.valueOf(movie.getBoxOffice()));
-                Attribute currency = new Attribute("currency", "$");
-                boxOffice.setAttribute(currency);
-
-                newMovie.addContent(title);
-                newMovie.addContent(cover);
-                newMovie.addContent(year);
-                newMovie.addContent(releaseDate);
-                newMovie.addContent(countries);
-                newMovie.addContent(director);
-                newMovie.addContent(cast);
-                newMovie.addContent(duration);
-                newMovie.addContent(distribution);
-                newMovie.addContent(languages);
-                newMovie.addContent(music);
-                newMovie.addContent(boxOffice);
-
-                root.addContent(newMovie);
+                root.addContent(getNewMovieNode(movie));
             }
         } catch (SaxonApiException e) {
             e.printStackTrace();
@@ -89,58 +50,64 @@ public class XMLManipulationLogic {
             XdmValue res = XPathFunctions.executeXpath(xp, "movies.xml");
             System.out.println(res.size());
             if (res.size() == 1) {
-                System.out.println(res.itemAt(0).getStringValue());
+                Document newDoc = XMLJDomFunctions.readStringToDocument(res.toString());
+                Element oldMovie = newDoc.getRootElement();
 
-
-//
-//                Element newMovie = new Element("movie");
-//                Element title = new Element("title").addContent(movie.getTitle());
-//                Element cover = new Element("cover").addContent(movie.getCover());
-//                Element year = new Element("year").addContent(String.valueOf(movie.getYear()));
-//                Element releaseDate = new Element("releaseDate").addContent(String.valueOf(movie.getReleaseDate()));
-//                Attribute releaseCountry = new Attribute("country", "USA");
-//                releaseDate.setAttribute(releaseCountry);
-//                Element countries = new Element("productionCountries");
-//                for (String s : movie.getCountry()) {
-//                    Element country = new Element("country").addContent(s);
-//                    countries.addContent(country);
-//                }
-//                Element director = new Element("director").addContent(movie.getDirector());
-//                Element cast = new Element("cast");
-//                for (String s : movie.getCast()) {
-//                    Element actor = new Element("actor").addContent(s);
-//                    cast.addContent(actor);
-//                }
-//                Element duration = new Element("duration").addContent(String.valueOf(movie.getDuration()));
-//                Element distribution = new Element("distribution").addContent(movie.getDistribution());
-//                Element languages = new Element("languages");
-//                for (String s : movie.getLanguage()) {
-//                    Element language = new Element("language").addContent(s);
-//                    languages.addContent(language);
-//                }
-//                Element music = new Element("music").addContent(movie.getMusic());
-//                Element boxOffice = new Element("boxOffice").addContent(String.valueOf(movie.getBoxOffice()));
-//                Attribute currency = new Attribute("currency", "$");
-//                boxOffice.setAttribute(currency);
-//
-//                newMovie.addContent(title);
-//                newMovie.addContent(cover);
-//                newMovie.addContent(year);
-//                newMovie.addContent(releaseDate);
-//                newMovie.addContent(countries);
-//                newMovie.addContent(director);
-//                newMovie.addContent(cast);
-//                newMovie.addContent(duration);
-//                newMovie.addContent(distribution);
-//                newMovie.addContent(languages);
-//                newMovie.addContent(music);
-//                newMovie.addContent(boxOffice);
-//
-//                root.addContent(newMovie);
+                if (doc.removeContent(oldMovie)) {
+                    root.addContent(getNewMovieNode(movie));
+                }
             }
         } catch (SaxonApiException e) {
             e.printStackTrace();
         }
         return doc;
+    }
+
+
+    private static Element getNewMovieNode(Movie movie) {
+        Element newMovie = new Element("movie");
+        Element title = new Element("title").addContent(movie.getTitle());
+        Element cover = new Element("cover").addContent(movie.getCover());
+        Element year = new Element("year").addContent(String.valueOf(movie.getYear()));
+        Element releaseDate = new Element("releaseDate").addContent(String.valueOf(movie.getReleaseDate()));
+        Attribute releaseCountry = new Attribute("country", "USA");
+        releaseDate.setAttribute(releaseCountry);
+        Element countries = new Element("productionCountries");
+        for (String s : movie.getCountry()) {
+            Element country = new Element("country").addContent(s);
+            countries.addContent(country);
+        }
+        Element director = new Element("director").addContent(movie.getDirector());
+        Element cast = new Element("cast");
+        for (String s : movie.getCast()) {
+            Element actor = new Element("actor").addContent(s);
+            cast.addContent(actor);
+        }
+        Element duration = new Element("duration").addContent(String.valueOf(movie.getDuration()));
+        Element distribution = new Element("distribution").addContent(movie.getDistribution());
+        Element languages = new Element("languages");
+        for (String s : movie.getLanguage()) {
+            Element language = new Element("language").addContent(s);
+            languages.addContent(language);
+        }
+        Element music = new Element("music").addContent(movie.getMusic());
+        Element boxOffice = new Element("boxOffice").addContent(String.valueOf(movie.getBoxOffice()));
+        Attribute currency = new Attribute("currency", "$");
+        boxOffice.setAttribute(currency);
+
+        newMovie.addContent(title);
+        newMovie.addContent(cover);
+        newMovie.addContent(year);
+        newMovie.addContent(releaseDate);
+        newMovie.addContent(countries);
+        newMovie.addContent(director);
+        newMovie.addContent(cast);
+        newMovie.addContent(duration);
+        newMovie.addContent(distribution);
+        newMovie.addContent(languages);
+        newMovie.addContent(music);
+        newMovie.addContent(boxOffice);
+
+        return newMovie;
     }
 }
