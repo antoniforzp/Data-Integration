5c6d8ede91ca913c95afcee1e61452906272948f
antek2137 <antonio25698@gmail.com>
2020-05-27 17:17:17 +0100

    generate done

--- Logic/src/main/java/model/logic/GenerateLogic.java
+++ Logic/src/main/java/model/logic/GenerateLogic.java
@@ -2,25 +2,41 @@ package model.logic;
 
 import model.LoadingTask;
 
+import java.io.File;
+import java.util.regex.Pattern;
+
 public class GenerateLogic {
 
     private static LoadingTask task;
 
-    public static void generate(String filename) {
-        task = new LoadingTask(filename);
-        new Thread(task).start();
+    public static boolean generate(String filename) {
+
+        if(checkFile(filename)){
+            task = new LoadingTask(filename);
+            new Thread(task).start();
+            return true;
+        }
+        return false;
     }
 
-    public static boolean cancel(){
-        if(task != null){
+    public static boolean cancel() {
+        if (task != null && task.isRunning()) {
             task.cancel();
             return true;
         }
         return false;
     }
 
-
     public static LoadingTask getTask() {
         return task;
+    }
+
+    private static boolean checkFile(String filename) {
+        if (!Pattern.matches(".*\\.xml", filename)) {
+            return false;
+        } else {
+            new File(filename);
+            return true;
+        }
     }
 }
